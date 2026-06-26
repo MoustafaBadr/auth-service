381b8c72ad9b38c915b1b2a88c5b50b5a6b12e75
Moustafa Badr <mostafabadr664@gmail.com>
2026-06-25 23:35:10 -0700

    test protection

--- src/main/java/com/mostafabadr/auth_service/AuthServiceApplication.java
+++ src/main/java/com/mostafabadr/auth_service/AuthServiceApplication.java
@@ -3,10 +3,14 @@ package com.mostafabadr.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
+
+    System.out.println("mostafaaaaaaaaaaaaaaaaaaaa");
     SpringApplication.run(AuthServiceApplication.class, args);
   }
 }
