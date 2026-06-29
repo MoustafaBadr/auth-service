d16a38bd0335ed960c50d63eceea56692c62104a
Moustafa Badr <mostafabadr664@gmail.com>
2026-06-29 15:50:18 -0700

    introduce role domain

---
+++ src/main/java/com/mostafabadr/authservice/authorization/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.mostafabadr.authservice.authorization.repository;
+
+import com.mostafabadr.authservice.authorization.entity.Role;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+  Optional<Role> findByName(String name);
+
+  boolean existsByName(String name);
+}
--- src/main/java/com/mostafabadr/authservice/common/entity/BaseEntity.java
+++ src/main/java/com/mostafabadr/authservice/common/entity/BaseEntity.java
@@ -1,31 +1,31 @@
 package com.mostafabadr.authservice.common.entity;
 
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
-
 import lombok.Getter;
-import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
-@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
+  @JdbcTypeCode(SqlTypes.BINARY)
+  @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
   private UUID id;
 
   @CreatedDate
-  @Column(nullable = false, updatable = false)
-  private LocalDateTime createdAt;
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
 
   @LastModifiedDate
-  @Column(nullable = false)
-  private LocalDateTime updatedAt;
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
 }
