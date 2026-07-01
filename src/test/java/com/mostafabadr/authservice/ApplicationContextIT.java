e04746a6083d7939bc76998fbe353d0bc2794d4c
Moustafa Badr <mostafabadr664@gmail.com>
2026-06-30 19:43:26 -0700

    refactor(test): migrate to Spring Boot Testcontainers integration

---
+++ src/test/java/com/mostafabadr/authservice/ApplicationContextIT.java
@@ -0,0 +1,10 @@
+package com.mostafabadr.authservice;
+
+import com.mostafabadr.authservice.common.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationContextIT extends IntegrationTest {
+
+  @Test
+  void contextLoads() {}
+}
