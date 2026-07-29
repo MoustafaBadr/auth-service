1f062857e2bf1b129ccbf41552fd23c84e6a0b21
Moustafa Badr <mostafabadr664@gmail.com>
2026-07-29 16:18:04 -0700

    organize test suites

--- src/test/java/com/mostafabadr/authservice/common/RepositoryIntegrationTest.java
+++ src/test/java/com/mostafabadr/authservice/common/RepositoryIntegrationTest.java
@@ -1,12 +1,14 @@
 package com.mostafabadr.authservice.common;
 
 import com.mostafabadr.authservice.common.config.JpaAuditingConfig;
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@Tag("repository")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @Import(JpaAuditingConfig.class)
